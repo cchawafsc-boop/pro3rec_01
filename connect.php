f0789486a6854576c0714e0fb44895d67dc29e9a
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-05-23 23:11:07 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -14,10 +14,4 @@ echo "Port: " . $port . "<br>";
 
 $conn=mysqli_connect($host,$user,$pass,$dbname, $port) or die("ไม่สามารถเชื่อมต่อฐานข้อมูลได้"); // เชื่อมต่อ ฐานข้อมูล
 
-if (!$conn) {
-    echo "Connection failed: " . mysqli_connect_error();
-} else {
-    echo "Connected successfully!";
-}
-
 ?>
