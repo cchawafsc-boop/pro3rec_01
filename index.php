f5c68c4345709ed03db35f309de500f628cbe1f4
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-05-24 20:54:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
+<?php
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+  session_start();
+  require('connect.php');
+  require('init_session.php');  
+?>
+
 <!doctype html>
 <head>
   <meta http-equiv="Content-Type" name="viewport" content="text/html; charset=utf-8; width=device-width; initial-scale=1.0" >
   <title>production record</title>
   <link rel="stylesheet" type='text/css' href="./style01.css">
-  
 </head>
 <body>
   <?php
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-    session_start();
-    require('connect.php');
-    require('init_session.php');
     require('topbar.php');
   ?>
   
