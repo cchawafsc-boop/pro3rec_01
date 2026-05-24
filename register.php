9bd369697062b6f4d5eefd03976d5d0b0ceaf7e4
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-05-24 22:14:50 +0700

    Update register.php

--- register.php
+++ register.php
@@ -15,8 +15,12 @@ echo "User_id: ".$Id."<br>";
 echo "Dep: ".$Dep."<br>";*/
 
 // Check the redundant user
-$sql = "SELECT * FROM `tb_user` WHERE `ID` LIKE \"".strval($Id)."\"";
-$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+//$sql = "SELECT * FROM `tb_user` WHERE `ID` LIKE \"".strval($Id)."\"";
+//$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+$stmt = mysqli_prepare($conn, "SELECT * FROM `tb_user` WHERE `ID` = ?");
+mysqli_stmt_bind_param($stmt, "s", $Id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 $n_row = mysqli_num_rows($result);
 $n_fld = mysqli_num_fields($result);
 
@@ -30,10 +34,14 @@ if($n_row>0){
     // AUT = 1 : 2nd authorized; able to read, write & delete
     // AUT = 2 : 3rd authorized; able to read & write
     // AUT = 3 : lowest authorized; able to read only
-    $sql = "INSERT INTO `tb_user`(`US`, `PW`, `ID`, `DEP`, `AUT`) ".
-              "VALUES (\"".$Us."\",\"".$Pw."\",\"".$Id."\",\"".$Dep."\",\"3\")";
-    $req = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-    if($req == 1){
+    //$sql = "INSERT INTO `tb_user`(`US`, `PW`, `ID`, `DEP`, `AUT`) ".
+    //          "VALUES (\"".$Us."\",\"".$Pw."\",\"".$Id."\",\"".$Dep."\",\"3\")";
+    //$req = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+    $aut = "3";
+    $stmt2 = mysqli_prepare($conn, "INSERT INTO `tb_user`(`US`, `PW`, `ID`, `DEP`, `AUT`) VALUES (?,?,?,?,?)");
+    mysqli_stmt_bind_param($stmt2, "sssss", $Us, $Pw, $Id, $Dep, $aut);
+    $req = mysqli_stmt_execute($stmt2);
+	if($req == 1){
         echo "<script> alert(\"ลงทะเบียนสำเร็จ\") </script>";
         echo "<script> location='index.php'</script>";
     }else{
