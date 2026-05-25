be0f53108f2736a52721045332681b3c3c2da909
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-05-25 23:05:25 +0700

    Update nie2_index.php

--- line_nie2/nie2_index.php
+++ line_nie2/nie2_index.php
@@ -2,10 +2,13 @@
 <head>
   <meta http-equiv="Content-Type" name="viewport" content="text/html; charset=utf-8; width=device-width; initial-scale=1.0" >
   <title>production record</title>
-  <link rel="stylesheet" type='text/css' href="./style01.css">
+  <link rel="stylesheet" type='text/css' href="../style01.css">
 </head>
 <body>
   <?php
+    session_start();
+    require('../connect.php');
+    require('../init_session.php');
     require('../topbar.php');
   ?>
   
