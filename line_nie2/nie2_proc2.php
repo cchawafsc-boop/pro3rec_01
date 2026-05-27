1e47d57ae9a845245bf63d3b20dac116b113b9f4
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-05-27 17:39:08 +0700

    Update nie2_proc2.php

--- line_nie2/nie2_proc2.php
+++ line_nie2/nie2_proc2.php
@@ -112,12 +112,12 @@
           </select>
         </div>
 
-        <div class="pro3-proc1-g-it" style="justify-content:flex-start;"><label>จำนวนใน Inv (pcs)</label></div>
+        <div class="pro3-proc1-g-it" style="font-size:75em;"><label>จำนวนใน Inv (pcs)</label></div>
         <div class="pro3-proc1-g-it">
           <input type="number" name="AmountInv" min="0" required>
         </div>
 
-        <div class="pro3-proc1-g-it" style="justify-content:flex-start;"><label>จำนวนสุ่ม (pcs)</label></div>
+        <div class="pro3-proc1-g-it" style="font-size:75em;"><label>จำนวนสุ่ม (pcs)</label></div>
         <div class="pro3-proc1-g-it">
           <input type="number" name="SamplingSize" min="0" required>
         </div>
