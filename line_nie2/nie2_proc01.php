bd2fdf3b1312b97e6b1af1c21d64d485ccbe80e1
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-06-01 22:17:49 +0700

    Update nie2_proc01.php

--- line_nie2/nie2_proc01.php
+++ line_nie2/nie2_proc01.php
@@ -16,10 +16,11 @@
         $boxJudge = $_POST['BoxJudge'];
         $lotID    = $_POST['LotID'];
         $lotIDFull= $lotID."_".$date."_".$time;
+        $done_f   = 'no';
 
         $stmt = mysqli_prepare($conn,
-            "INSERT INTO `tb_proc1` (`ProdName`,`InvNo`,`WO`,`SubLot`,`Date`,`Time`,`Opr`,`AppCheck`,`BoxQty`,`BoxJudge`,`LotID`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
-        mysqli_stmt_bind_param($stmt, "ssssssssiss", $prodName, $invNo, $wo, $subLot, $date, $time, $opr, $appCheck, $boxQty, $boxJudge, $lotIDFull);
+            "INSERT INTO `tb_proc1` (`ProdName`,`InvNo`,`WO`,`SubLot`,`Date`,`Time`,`Opr`,`AppCheck`,`BoxQty`,`BoxJudge`,`LotID`,`DoneFlag`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+        mysqli_stmt_bind_param($stmt, "ssssssssisss", $prodName, $invNo, $wo, $subLot, $date, $time, $opr, $appCheck, $boxQty, $boxJudge, $lotIDFull, $done_f);
         $req = mysqli_stmt_execute($stmt);
         if ($req) {
             echo "<script>alert('บันทึกข้อมูลสำเร็จ'); location='./nie2_index.php';</script>";
