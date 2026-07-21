f82a46fc3f18fa48c2e85acc4277b54223ff58bf
cchawafsc-boop <c.chawa.fsc@gmail.com>
2026-07-21 23:23:22 +0700

    Add files via upload

---
+++ line_nie2/ngmode.php
@@ -0,0 +1,6 @@
+<?php
+$ngModeList = [
+        'Blister','Break','Bumps','Burrs','Chip','Crack','Contam','Discolor',
+        'Dent','Scratch','Scuff','Stain','Exposed','Pitting','Deform','Finger'
+    ];
+?>
